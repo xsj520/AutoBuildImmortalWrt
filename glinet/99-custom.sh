09299dae3d4133dd024d2985b40c79bd408856aa
xsj520 <72717602+xsj520@users.noreply.github.com>
2025-08-17 22:26:40 +0800

    Update 99-custom.sh

--- glinet/99-custom.sh
+++ glinet/99-custom.sh
@@ -105,4 +105,23 @@ FILE_PATH="/etc/openwrt_release"
 NEW_DESCRIPTION="Packaged by wukongdaily"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
+sed -i 's/"网络存储"/"存储"/g' `grep "网络存储" -rl ./`
+sed -i 's/"HomeProxy"/"科学上网"/g' `grep "HomeProxy" -rl ./`
+sed -i 's/"启动项"/"启动项目"/g' `grep "启动项" -rl ./`
+sed -i 's/"挂载点"/"挂载地点"/g' `grep "挂载点" -rl ./`
+sed -i 's/"重启"/"重启开关"/g' `grep "重启" -rl ./`
+sed -i 's/"主机名"/"主机名称"/g' `grep "主机名" -rl ./`
+sed -i 's/"接口"/"接口设置"/g' `grep "接口" -rl ./`
+sed -i 's/"无线"/"无线设置"/g' `grep "无线" -rl ./`
+sed -i 's/"管理权"/"管理设置"/g' `grep "管理权" -rl ./`
+sed -i 's/"软件包"/"软件包名"/g' `grep "软件包" -rl ./`
+sed -i 's/"UPnP IGD 和 PCP"/"upnp设置"/g' `grep "UPnP IGD 和 PCP" -rl ./`
+sed -i 's/"动态 DNS"/"动态DNS"/g' `grep "动态 DNS" -rl ./`
+sed -i 's/"路由表"/"路由器表"/g' `grep "路由表" -rl ./`
+sed -i 's/"路由"/"路由设置"/g' `grep "路由" -rl ./`
+sed -i 's/"备份与升级"/"备份升级"/g' `grep "备份与升级" -rl ./`
+sed -i 's/"FileBrowser"/"文件管理"/g' `grep "FileBrowser" -rl ./`
+sed -i 's/"终端"/"终端命令"/g' `grep "终端" -rl ./`
+sed -i 's/"防火墙"/"防火墙区"/g' `grep "防火墙" -rl ./`
+
 exit 0
